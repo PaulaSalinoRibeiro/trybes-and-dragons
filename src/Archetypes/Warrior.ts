9dc946628a2ebefcca862ff07a34b2f59a6c07b5
Paula <paulasalinor@gmail.com>
2022-08-09 18:31:17 -0300

    add class Warrior

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,25 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Warrior extends Archetype {
+  private _energyType: EnergyType;
+  private static _count = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+    Warrior._count += 1;
+  }
+
+  get name(): string {
+    return this.name;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances() : number {
+    return Warrior._count;
+  }
+}
