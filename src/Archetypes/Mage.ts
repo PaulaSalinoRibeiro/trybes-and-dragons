43283a2254994d855566483457f719d557a2698f
Paula <paulasalinor@gmail.com>
2022-08-09 18:26:53 -0300

    add class Mage

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,25 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Mage extends Archetype {
+  private _energyType: EnergyType;
+  private static _count = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'mana';
+    Mage._count += 1;
+  }
+
+  get name(): string {
+    return this.name;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Mage._count;
+  }
+}
