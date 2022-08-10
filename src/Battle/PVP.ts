77f35c60b1fabf666b16f1607aad774832402c65
Paula <paulasalinor@gmail.com>
2022-08-10 17:23:46 -0300

    fix fight method in PVP class

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -14,10 +14,11 @@ export default class PVP extends Battle {
     this._player2 = player2;
   }
 
-  fight() {
-    this._player1.attack(this._player2);
-    this._player2.attack(this._player1);
-    
-    return this.player.lifePoints === -1 ? -1 : 1;
+  public fight() {
+    while (this._player1.lifePoints > 0 && this._player2.lifePoints > 0) {
+      this._player1.attack(this._player2);
+      this._player2.attack(this._player1);
+    }
+    return super.fight();
   }
 }
