dd1602704974e5aa837a93b1fcbaec21d5c09029
Paula <paulasalinor@gmail.com>
2022-08-10 17:24:11 -0300

    fix fight method in PVE class

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -1,20 +1,29 @@
 import Fighter, { SimpleFighter } from '../Fighter';
+import Monster from '../Monster';
 import Battle from './Battle';
 
 export default class PVE extends Battle {
   _player1: Fighter;
-  _player2: SimpleFighter[];
+  _player2: Array<SimpleFighter | Monster>;
 
-  constructor(player1: Fighter, player2: SimpleFighter[]) {
+  constructor(player1: Fighter, player2: Array<SimpleFighter | Monster>) {
     super(player1);
     this._player1 = player1;
     this._player2 = player2;
   }
 
+  findAliveSimpleFighter(): SimpleFighter | undefined {
+    return this._player2
+      .find((player2) => player2.lifePoints > 0);
+  }
+
   fight(): number {
-    this._player1.attack(this._player2[0]);
-    this._player2[0].attack(this._player1);
-    
-    return this.player.lifePoints === -1 ? -1 : 1;
+    let player = this.findAliveSimpleFighter();
+    while (this._player1.lifePoints > 0 && player !== undefined) {
+      this._player1.attack(player);
+      player.attack(this._player1);
+      player = this.findAliveSimpleFighter();
+    }
+    return super.fight();
   }
 }
