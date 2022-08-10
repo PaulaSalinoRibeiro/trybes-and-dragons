e667aaae388550278a2663f0660b411359d4642e
Paula <paulasalinor@gmail.com>
2022-08-10 13:21:37 -0300

    refactory PVP class

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -15,10 +15,9 @@ export default class PVP extends Battle {
   }
 
   fight() {
-    while (this._player1.lifePoints !== -1 && this._player2.lifePoints !== -1) {
-      this._player1.attack(this._player2);
-      this._player2.attack(this._player1);
-    }
+    this._player1.attack(this._player2);
+    this._player2.attack(this._player1);
+    
     return this.player.lifePoints === -1 ? -1 : 1;
   }
 }
